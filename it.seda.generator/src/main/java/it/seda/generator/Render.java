5a3a14b68297595d218c4440690ffb30ce5c198f
raffaello.ponzelli <raffaello.ponzelli@seda.it>
2013-11-25 12:26:51 +0100

    classe render per generator

--- it.seda.generator/src/main/java/it/seda/generator/Render.java
+++ it.seda.generator/src/main/java/it/seda/generator/Render.java
@@ -1,9 +1,20 @@
 package it.seda.generator;
 
+import org.apache.velocity.app.Velocity;
+
+
+
+
 public class Render {
+	
+	private Velocity velocity;
+	private Context context;
+	private Writer writer;
+	private InputStream inputStream;
 
 	public Render() {
-		// TODO Auto-generated constructor stub
+		velocity.evaluate(context, writer, logTag, inputStream)
+		
 	}
 
 }
