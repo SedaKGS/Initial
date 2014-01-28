74e6114f4342e80f26cd0098a847e90ed38d52d2
f.ricci <f.ricci@seda.it>
2014-01-28 13:25:59 +0100

    cas go on!

--- it.seda.security.cas/src/main/java/it/seda/security/cas/manager/controller/CustomerController.java
+++ it.seda.security.cas/src/main/java/it/seda/security/cas/manager/controller/CustomerController.java
@@ -3,15 +3,20 @@
  */
 package it.seda.security.cas.manager.controller;
 
+import javax.validation.Valid;
+
 import it.seda.jdbc.commons.DataPage;
 import it.seda.security.domain.Customer;
 import it.seda.security.service.ManagerService;
+import it.seda.sem.mvc.manager.models.FormClient;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,6 +43,18 @@ public class CustomerController {
 		Customer customerForm=new Customer();
 		refreshDatagrid(model, pageNumber, rowsPerPage);
 		model.addAttribute("customerForm", customerForm);
+
+		return "customer_manager";
+	}	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String post(@Valid @ModelAttribute("customerForm") Customer customerForm,
+			BindingResult result,
+			@RequestParam(value="pageNumber", defaultValue="1") int pageNumber,
+			@RequestParam(value="rowsPerPage",defaultValue="15") int rowsPerPage,
+			ModelMap model) {
+
+
 
 		return "customer_manager";
 	}	
--- it.seda.security.cas/src/main/java/it/seda/security/cas/manager/forms/CutomerForm.java
+++ it.seda.security.cas/src/main/java/it/seda/security/cas/manager/forms/CutomerForm.java
@@ -2,4 +2,8 @@ package it.seda.security.cas.manager.forms;
 
 public class CutomerForm {
 
+	private String id;
+	private String name;
+	private String code;
+	
 }
